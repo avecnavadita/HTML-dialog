b908ef195031b2be2d4ab075d63130ec086be805
avecnavadita <82716581+avecnavadita@users.noreply.github.com>
2021-04-18 22:08:29 +0545

    changes in CSS, JS
    - added function to hide dialog
    - fixed CSS
    - added google icons and fonts

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,16 @@
+// displays the dialog 
 function showLogin(){
     // 1 find the dialog box (DOM)-document object modal
     // since getElements returns a collection, we only take the first element using 0 as index
     const el = document.getElementsByTagName('dialog')[0];
     // 2 add open attribute to the dialog
     el.setAttribute("open", "");
+}
+
+// hides the dialog 
+function hideLogin(){
+    // 1 find the dialog box 
+    const dialog = document.getElementsByTagName('dialog')[0];
+    // 2 remove open attribute from dialog box
+    dialog.removeAttribute("open");
 }
